905b8709e28ebabacc1edddc779a5ab1cb21246b
dark567 <darkmit@list.ru>
2020-01-25 21:02:09 +0200

    20200125_05

--- ALevelProject/LabTest/Controllers/HomeController.cs
+++ ALevelProject/LabTest/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LabTest.Filters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,7 @@ namespace LabTest.Controllers
 {
     public class HomeController : Controller
     {
+
         public ActionResult Index()
         {
             return View();
---
+++ CustomErrors/Web/Web/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static int[] ids = {1, 2, 3, 4, 5};
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetArticle(int id)
+        {
+            //HandleErrorAttribute
+            if (ids.All(x => x != id))
+                return HttpNotFound();
+            return View();
+        }
+
+        public ActionResult GetNews(int id)
+        {
+
+            if (ids.All(x => x != id))
+                throw new HttpException(404, "Not found!");
+            return View("GetArticle");
+        }
+    }
+}
