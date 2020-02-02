d6b1a3504d3f32a133eb43567dedf7ccb10ee4e8
dark567 <darkmit@list.ru>
2020-02-02 21:07:54 +0200

    20200202_01

--- ALevelProject/DataLayer/Entityes/JorOrder.cs
+++ ALevelProject/DataLayer/Entityes/JorOrder.cs
@@ -18,6 +18,9 @@ namespace DataLayer.Entityes
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         [Display(Name = "DateAdd")]
         public DateTime DateAdd { get; set; }
+        [StringLength(10, MinimumLength = 4, ErrorMessage = "The name cannot be longer than 10 characters and less than 4.")]
+        [Display(Name = "Num")]
+        public string Num { get; set; }
         public DicClient ClientId { get; set; }
         public DicGood GoodId { get; set; }
         public int Value { get; set; }
---
+++ ALevelProject/LabTest/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LabTest.Controllers
+{
+    public class TestController : Controller
+    {
+        // создаем контекст данных
+        EFDBContext db = new EFDBContext();
+
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View(db.DicClient.ToList());
+        }
+    }
+}
