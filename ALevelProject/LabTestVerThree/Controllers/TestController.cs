5a3e8844acdc5d7d23cf72a18b9b693caad65ed0
dark567 <darkmit@list.ru>
2020-02-09 21:54:29 +0200

    20200209_04

---
+++ ALevelProject/LabTestVerThree/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LabTestVerThree.Controllers
+{
+    public class TestController : Controller
+    {
+        // создаем контекст данных
+        EFDBContext db = new EFDBContext();
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View(db.DicClient.ToList());
+        }
+    }
+}
--- ALevelProject/LabTestVerThree/NavbaR/Navbar.cs
+++ ALevelProject/LabTestVerThree/NavbaR/Navbar.cs
@@ -14,13 +14,13 @@ namespace LabTestVerThree.NavbaR
             //topNav.Add(new NavbarItem() { Id = 2, action = "Contact", nameOption = "Contact", controller = "Home", isParent = false, parentId = -1 });
             // drop down Menu 
             topNav.Add(new NavbarItem() { Id = 1, action = "Reports", nameOption = "Журналы", controller = "ReportGen", isParent = true, parentId = -1 });
-            topNav.Add(new NavbarItem() { Id = 2, action = "SummaryReport", nameOption = "Заказы", controller = "ReportGen", isParent = false, parentId = 1 });
-            topNav.Add(new NavbarItem() { Id = 3, action = "DailyReport", nameOption = "Заполнение результатов", controller = "ReportGen", isParent = false, parentId = 1 });
-            topNav.Add(new NavbarItem() { Id = 4, action = "MonthlyReport", nameOption = "Результаты", controller = "ReportGen", isParent = false, parentId = 1 });
+            topNav.Add(new NavbarItem() { Id = 2, action = "Index", nameOption = "Заказы", controller = "JorOrders", isParent = false, parentId = 1 });
+            topNav.Add(new NavbarItem() { Id = 3, action = "Index", nameOption = "Заполнение результатов", controller = "JorAddResults", isParent = false, parentId = 1 });
+            topNav.Add(new NavbarItem() { Id = 4, action = "Index", nameOption = "Результаты", controller = "ReportGen", isParent = false, parentId = 1 });
             // drop down Menu 
             topNav.Add(new NavbarItem() { Id = 5, action = "Reports", nameOption = "Справочники", controller = "ReportGen", isParent = true, parentId = -1 });
             topNav.Add(new NavbarItem() { Id = 6, action = "Index", nameOption = "Клиенты", controller = "DicClients", isParent = false, parentId = 5 });
-            topNav.Add(new NavbarItem() { Id = 7, action = "DailyReport", nameOption = "Услуги", controller = "ReportGen", isParent = false, parentId = 5 });
+            topNav.Add(new NavbarItem() { Id = 7, action = "Index", nameOption = "Услуги", controller = "DicGoods", isParent = false, parentId = 5 });
             topNav.Add(new NavbarItem() { Id = 8, action = "MonthlyReport", nameOption = "Month Report", controller = "ReportGen", isParent = false, parentId = 5 });
             // drop down Menu 
             topNav.Add(new NavbarItem() { Id = 9, action = "Reports", nameOption = "Reports", controller = "ReportGen", isParent = true, parentId = -1 });
