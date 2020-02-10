5769bf99800950f5016293b34c8baf9ffc5b8034
dark567 <darkmit@list.ru>
2020-02-10 17:46:04 +0200

    20200210_00

--- ALevelProject/LabTestVerThree/Controllers/DicClientsController.cs
+++ ALevelProject/LabTestVerThree/Controllers/DicClientsController.cs
@@ -186,7 +186,7 @@ namespace LabTestVerThree.Controllers
             return View(dicClient);
         }
 
-        // POST: DicClient/Edit/5
+
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost, ActionName("Edit")]
--- ALevelProject/LabTestVerThree/Controllers/JorOrdersController.cs
+++ ALevelProject/LabTestVerThree/Controllers/JorOrdersController.cs
@@ -93,6 +93,212 @@ namespace LabTestVerThree.Controllers
             return View(jorOrder);
         }
 
+        public ActionResult Create_(Guid? id, bool? saveChangesError = false)
+        {
+            if (id != null)
+            {
+                DicClient dicClient = db.DicClient.Find(id);
+
+                if (ModelState.IsValid)
+                {
+                    ViewBag.Client = dicClient;
+                }
+                else
+                {
+                    return PartialView("ListClients_");
+                }
+            }
+            
+
+
+
+            return View();
+        }
+
+        // POST: JorOrder/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create_(Guid? id)
+        {
+            try
+            {
+                DicClient dicClient = db.DicClient.Find(id);
+
+                if (ModelState.IsValid)
+                {
+                    ViewBag.Client = dicClient;
+                }
+                else
+                {
+                    return PartialView("ListClients_");
+                }
+            }
+            catch (RetryLimitExceededException /* dex */)
+            {
+                //Log the error (uncomment dex variable name and add a line here to write a log.
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+            }
+            //return View(client);
+            return RedirectToAction("Create_");
+        }
+
+        public ActionResult ListClients(string sortOrder, string currentFilter, string searchString, int? page)
+        {
+
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewBag.DateSortParm = sortOrder == "Date" ? "date_desc" : "Date";
+
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewBag.CurrentFilter = searchString;
+
+            IEnumerable<DicClient> clients = from s in db.DicClient
+                                             select s;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                clients = clients.Where(s => s.Name.ToLower().Contains(searchString.ToLower())
+                                       || s.Surname.ToLower().Contains(searchString.ToLower()));
+            }
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    clients = clients.OrderByDescending(s => s.Name);
+                    break;
+                case "Date":
+                    clients = clients.OrderBy(s => s.BirthDate);
+                    break;
+                case "date_desc":
+                    clients = clients.OrderByDescending(s => s.BirthDate);
+                    break;
+                default:  // Name ascending 
+                    clients = clients.OrderBy(s => s.Name);
+                    break;
+            }
+
+            int pageSize = 5; /*!!!!*/
+            int pageNumber = (page ?? 1);
+            //return View(clients.ToPagedList(pageNumber, pageSize));
+
+            //// получаем из бд все объекты Book
+            //IEnumerable<DicClient> dicClients = db.DicClient;
+            //// передаем все объекты в динамическое свойство Books в ViewBag
+            //ViewBag.DicClients = dicClients;
+            //// возвращаем представление
+            //return View();
+            return PartialView("ListClients_", clients.ToPagedList(pageNumber, pageSize));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ListClients(DicClient client)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.DicClient.Add(client);
+                    db.SaveChanges();
+                    return Json(new { success = true });
+                }
+                else
+                {
+                    return PartialView("ListClients_", client);
+                }
+            }
+            catch (RetryLimitExceededException /* dex */)
+            {
+                //Log the error (uncomment dex variable name and add a line here to write a log.
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+            }
+            return View(client);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ListClient(Guid? id)
+        {
+            try
+            {
+                DicClient dicClient = db.DicClient.Find(id);
+
+                if (ModelState.IsValid)
+                {
+                    ViewBag.Client = dicClient;
+                }
+                else
+                {
+                    return PartialView("ListClients_");
+                }
+            }
+            catch (RetryLimitExceededException /* dex */)
+            {
+                //Log the error (uncomment dex variable name and add a line here to write a log.
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+            }
+            //return View(client);
+            return RedirectToAction("Create_");
+        }
+        //// POST: JorOrder/Create
+        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        //// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult ListClient(string sortOrder, string currentFilter, string searchString, int? page)
+        //{
+
+        //    ViewBag.CurrentSort = sortOrder;
+        //    ViewBag.DateAddortParm = sortOrder == "dateAdd" ? "num" : "name";
+        //    ViewBag.NumSortParm = String.IsNullOrEmpty(sortOrder) ? "num" : "";
+
+        //    if (searchString != null)
+        //    {
+        //        page = 1;
+        //    }
+        //    else
+        //    {
+        //        searchString = currentFilter;
+        //    }
+
+        //    ViewBag.CurrentFilter = searchString;
+
+        //    IEnumerable<JorOrder> good = from s in db.JorOrder
+        //                                 select s;
+        //    if (!String.IsNullOrEmpty(searchString))
+        //    {
+        //        good = good.Where(s => s.Num.ToLower().Contains(searchString.ToLower())
+        //                               || s.DateAdd.Equals(searchString));
+        //    }
+        //    switch (sortOrder)
+        //    {
+        //        case "сode":
+        //            good = good.OrderByDescending(s => s.DateAdd);
+        //            break;
+        //        case "name":
+        //            good = good.OrderBy(s => s.Num);
+        //            break;
+        //        case "code_name":
+        //            good = good.OrderByDescending(s => s.DateAdd);
+        //            break;
+        //        default:  // Name ascending 
+        //            good = good.OrderBy(s => s.Num);
+        //            break;
+        //    }
+
+        //    int pageSize = 5; /*!!!!*/
+        //    int pageNumber = (page ?? 1);
+        //    return View(good.ToPagedList(pageNumber, pageSize));
+        //}
+
         // GET: JorOrder/Details/5
         public ActionResult Details(Guid? id)
         {
